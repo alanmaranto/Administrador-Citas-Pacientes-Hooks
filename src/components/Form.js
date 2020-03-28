01cfdda92e9be03843f1a72011d0880002001f62
alan <alanarostegui@hotmail.com>
2020-03-27 19:34:00 -0600

    feature(form): add fields

--- src/components/Form.js
+++ src/components/Form.js
@@ -1,9 +1,40 @@
-import React from 'react';
+import React from "react";
 
 const Form = () => {
-    return ( 
-        <h1>Desde form</h1>
-     );
-}
- 
+  return (
+    <>
+      <h2>Crear Cita</h2>
+      <form>
+        <label htmlFor="">Nombre Mascota</label>
+        <input
+          type="text"
+          name="pet"
+          className="u-full-width"
+          placeholder="Nombre mascota"
+        />
+        <label htmlFor="">Nombre Dueño</label>
+        <input
+          type="text"
+          name="owner"
+          className="u-full-width"
+          placeholder="Nombre dueño de la mascota"
+        />
+        <label htmlFor="">Fecha</label>
+        <input type="date" name="date" className="u-full-width" />
+        <label htmlFor="">Hora</label>
+        <input type="time" name="time" className="u-full-width" />
+        <label htmlFor="">Síntomas</label>
+        <textarea
+          name="symptom"
+          className="u-full-width"
+        />
+        <button 
+            type="submit"
+            className="u-full-width button-primary"
+        >Agregar Cita</button>
+      </form>
+    </>
+  );
+};
+
 export default Form;
