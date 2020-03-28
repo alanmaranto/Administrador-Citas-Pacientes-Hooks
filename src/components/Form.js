f87b1a03a919452d5af1d4e781af7b8f314589ba
alan <alanarostegui@hotmail.com>
2020-03-27 20:35:24 -0600

    feature(form): add state and onchange function

--- src/components/Form.js
+++ src/components/Form.js
@@ -1,37 +1,61 @@
-import React from "react";
+import React, { useState } from "react";
+
+const initialState = {
+  pet: "",
+  owner: "",
+  date: "",
+  time: "",
+  symptom: ""
+};
 
 const Form = () => {
+  const [appointment, setAppointment] = useState(initialState);
+
+  const onChangeAppointment = () => console.log("Escribiendo");
+
   return (
     <>
       <h2>Crear Cita</h2>
       <form>
-        <label htmlFor="">Nombre Mascota</label>
+        <label>Nombre Mascota</label>
         <input
           type="text"
           name="pet"
           className="u-full-width"
           placeholder="Nombre mascota"
+          onChange={onChangeAppointment}
         />
-        <label htmlFor="">Nombre Dueño</label>
+        <label>Nombre Dueño</label>
         <input
           type="text"
           name="owner"
           className="u-full-width"
           placeholder="Nombre dueño de la mascota"
+          onChange={onChangeAppointment}
         />
-        <label htmlFor="">Fecha</label>
-        <input type="date" name="date" className="u-full-width" />
-        <label htmlFor="">Hora</label>
-        <input type="time" name="time" className="u-full-width" />
-        <label htmlFor="">Síntomas</label>
+        <label>Fecha</label>
+        <input
+          type="date"
+          name="date"
+          className="u-full-width"
+          onChange={onChangeAppointment}
+        />
+        <label>Hora</label>
+        <input
+          type="time"
+          name="time"
+          className="u-full-width"
+          onChange={onChangeAppointment}
+        />
+        <label>Síntomas</label>
         <textarea
           name="symptom"
           className="u-full-width"
+          onChange={onChangeAppointment}
         />
-        <button 
-            type="submit"
-            className="u-full-width button-primary"
-        >Agregar Cita</button>
+        <button type="submit" className="u-full-width button-primary">
+          Agregar Cita
+        </button>
       </form>
     </>
   );
