11df93a9f6add852fe17175f19e65bb0cd976549
alan <alanarostegui@hotmail.com>
2020-03-28 02:27:02 -0600

    refactor: containter pattern

--- src/index.js
+++ src/index.js
@@ -1,11 +1,11 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
-import App from './App';
+import Appointment from './containers/AppointmentContainer'
 
 ReactDOM.render(
   <React.StrictMode>
-    <App />
+    <Appointment />
   </React.StrictMode>,
   document.getElementById('root')
 );
