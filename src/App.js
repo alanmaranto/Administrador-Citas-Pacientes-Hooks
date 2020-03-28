09d782ddf525dcfe2fe25d59995f0b0f15938d59
alan <alanarostegui@hotmail.com>
2020-03-27 19:14:08 -0600

    wip(form)

--- src/App.js
+++ src/App.js
@@ -1,8 +1,19 @@
-import React from 'react';
+import React from "react";
+import Form from './components/Form'
 
 function App() {
   return (
-    <h1>Administrador de pacientes</h1>
+    <>
+      <h1>Administrador de pacientes</h1>
+      <div className="container">
+        <div className="row">
+          <div className="one-half column">
+            <Form />
+          </div>
+          <div className="one-half column">2</div>
+        </div>
+      </div>
+    </>
   );
 }
 
